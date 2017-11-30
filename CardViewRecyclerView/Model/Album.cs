5617449705d1d8389ce96181d469ad83862e7dbb
John Richard <johnjake_talledo2000@yahoo.com>
2017-11-30 11:14:41 -0800

    Installed Dependency - AppCompat 25.2+, RecyclerView, CardView, Support.Design, Square Picasso

---
+++ CardViewRecyclerView/Model/Album.cs
@@ -0,0 +1,9 @@
+﻿namespace CardViewRecyclerView.Model
+{
+    public class Album
+    {
+        public string Name { get; set; }
+        public int NumOfSongs { get; set; }
+        public int Thumbnail { get; set; }
+    }
+}
